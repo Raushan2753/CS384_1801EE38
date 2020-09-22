2ead4e61eeba2c030aaf3e5e8cbbd88b93603c41
Raushan Kumar <raushanrock2753@gmail.com>
2020-09-22 23:16:29 +0530

    Created the Mean Function in Tutorial 2 of Assignment 2

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -3,6 +3,11 @@
 # Function to compute mean
 def mean(first_list):
     # mean Logic
+    if(len(first_list)==0):
+        return 0
+    summation_value = summation(first_list)
+    mean_value = summation_value/len(first_list)
+    mean_value = round(mean_value,3)
     return mean_value
 
 
