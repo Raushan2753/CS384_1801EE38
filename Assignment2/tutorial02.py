55ea3bdb44387d23a13f622a34a1bcece34e019a
Raushan Kumar <raushanrock2753@gmail.com>
2020-09-23 21:03:06 +0530

    Created Standard Deviation Function in Tutorial 2 of Assignment 2

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -20,6 +20,11 @@ def median(first_list):
 # Function to compute Standard deviation. You cant use Python functions
 def standard_deviation(first_list):
     # Standard deviation Logic
+    if (len(first_list)==0):
+        return 0
+    variance_value = variance(first_list)
+    standard_deviation_value = math.sqrt(variance_value)
+    standard_deviation_value = round(standard_deviation_value,6)
     return standard_deviation_value
 
 
