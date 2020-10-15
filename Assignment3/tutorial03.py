008b36be1d793bc3a9d2e89978997771e3297199
Raushan Kumar <raushanrock2753@gmail.com>
2020-10-15 22:05:22 +0530

    Created the state Function in Assignment 3!

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -236,9 +236,36 @@ def dob():
 
 
 
+
 def state():
     # Read csv and process
+    file = open('studentinfo_cs384.csv', 'r')
+    with file:
+        stud_file = csv.DictReader(file,skipinitialspace = True)
+        header = ['id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state']
+        cd = os.getcwd()
+        path_analytics = os.path.join(cd,'analytics')
+        if(os.path.exists(path_analytics)==False):
+            os.mkdir(path_analytics)
+        state_path = os.path.join(path_analytics,'state')
+        if(os.path.exists(state_path)==False):
+            os.mkdir(state_path)
+        for row in stud_file:
+            state_name = row['state']
+            state_name = state_name.lower()
+            csv_state = state_name + '.csv'
+            file_path = os.path.join(state_path,csv_state)
+            if (os.path.exists(file_path) == False):
+                with open(file_path,'a') as file1:
+                    writer = csv.DictWriter(file1, fieldnames = header)
+                    writer.writeheader()
+                    writer.writerow(row)
+            else :
+                with open(file_path, 'a') as file2 :
+                    writer = csv.DictWriter(file2, fieldnames = header)
+                    writer.writerow(row)
     pass
+
 
 
 def blood_group():
