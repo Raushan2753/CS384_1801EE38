61996210b00a256db45c3c18f04ab97e3e36c723
Raushan Kumar <raushanrock2753@gmail.com>
2020-11-15 22:52:37 +0530

    Created the Function rename_Game_of_Thrones in Assignment 5

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -25,9 +25,29 @@ def rename_FIR(paddingepisode, paddingseason=0):
     pass
 
 
-def rename_Game_of_Thrones(folder_name):
-    # rename Logic 
-    
+def rename_Game_of_Thrones(paddingseason, paddingepisode):
+    # rename Logic
+    series_path = r'./Subtitles/Game of Thrones'
+    file_compile = os.listdir(series_path)
+    for series in file_compile:
+        split_series = series.split(' - ')
+        season_no = split_series[1][0:split_series[1].index('x')]
+        episode_no = split_series[1][split_series[1].index('x')+1:]
+        episode_name = split_series[2][0:split_series[2].index('.')]
+        extension = (re.split(r'\.',split_series[2])[-1]).strip()
+        while(len(season_no) < paddingseason):
+            season_no = '0'+season_no
+        if(len(season_no) > paddingseason):
+            season_no = season_no[-1*paddingseason:]
+        while(len(episode_no) < paddingepisode):
+            episode_no = '0'+episode_no
+        if(len(episode_no) > paddingepisode):
+            episode_no = episode_no[-1*paddingepisode:]
+        try:
+            os.rename(series_path+'/'+series, series_path+'/' +split_series[0]+' - Season '+season_no+' Episode '+episode_no+' - '+episode_name+'.'+extension)
+        except:
+            os.remove(series_path+'/'+series)
+    pass
 
 def rename_Sherlock(folder_name):
     # rename Logic 
